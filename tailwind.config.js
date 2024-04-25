ae146237647fff7056200f543ded02b0a93752e9
Abhinav Yadav <abhinavay2003@gmail.com>
2024-04-25 14:39:15 +0530

    Update package-lock.json, package.json, index.css, and tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,12 @@ export default {
   darkMode: "class",
   theme: {
     extend: {
+     colors: {
+        bgc: "var(--color-grey-100)",
+        contentBgc: "var(--color-grey-50)",
+        textColor: "var(--color-grey-600)",
+        blue: "var(--color-blue)",
+      },
     },
   },
   plugins: [require("tailwind-scrollbar-hide")],
